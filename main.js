35099abf0c26a2201e6ed49763b3c949c5fcfa14
adhritdc <82810967+adhritdc@users.noreply.github.com>
2022-03-12 12:19:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,26 +14,20 @@ function gotResults(error, results)
     console.log(results); 
     document.getElementById("result_label").innerHTML = results[0].label; 
     document.getElementById("result_confidence").innerHTML = (results[0].confidence*100).toFixed(1)+"%";
+    img1 = document.getElementById("imagePosition");
     if (results[0].label == "Barking")
     {
-        img1 = document.getElementById("imagePosition");
-        img1.src("bark.gif");
+        img1.src="bark.gif";
     }
     else if (results[0].label == "Meawing")
     {
-        img2 = document.getElementById("imagePosition");
-        img2.src("meaw.gif");
+        img1.src="meow.gif";
     }
     else
     {
-        img3 = document.getElementById("imagePosition");
-        img3.src("listen.gif");
+        img1.src="listen.gif";
     }
     }
 }
 var cat = 0;
 var dog = 0;
-
-red = Math.floor(Math.random()*255) + 1;
-green = Math.floor(Math.random()*255) + 1;
-blue = Math.floor(Math.random()*255) + 1;
